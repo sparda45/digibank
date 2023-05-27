1dad8b8c58d86d75fef836a1c75926dfdb5db000
Sheren Ming Sianggara <sherenxxxianggara@gmail.com>
2023-05-27 23:09:50 +0700

    ubahlagi

--- src/constants/index.js
+++ src/constants/index.js
@@ -25,21 +25,21 @@ export const features = [
     icon: star,
     title: "Rewards",
     content:
-      "The best credit cards offer some tantalizing combinations of promotions and prizes",
+      "Kartu kredit terbaik menawarkan kombinasi menarik dari promosi dan hadiah-hadiah.",
   },
   {
     id: "feature-2",
     icon: shield,
     title: "100% Secured",
     content:
-      "We take proactive steps make sure your information and transactions are secure.",
+      "Kami mengambil langkah-langkah proaktif untuk memastikan informasi dan transaksi Anda aman.",
   },
   {
     id: "feature-3",
     icon: send,
     title: "Balance Transfer",
     content:
-      "A balance transfer credit card can save you a lot of money in interest charges.",
+      "Kartu kredit transfer saldo dapat menghemat banyak uang Anda dalam biaya bunga.",
   },
 ];
 
