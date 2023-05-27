bc4b5ae9a5f351a2ff1c48e3447a39e487a4f9f3
jovancamorris <you@example.comjovancamoris@gmail.com>
2023-05-27 22:56:54 +0700

    cta lgi

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,7 +4,7 @@ import Button from "./Button";
 const CTA = () => (
   <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
     <div className="flex-1 flex flex-col">
-      <h2 className={styles.heading2}>Penasaran dengan service kami ? Ayo pesan sekarang ?</h2>
+      <h2 className={styles.heading2}>Penasaran dengan service kami ? Ayo pesan sekarang !</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
       Semua yang Anda perlukan untuk menerima pembayaran digital dan mengembangkan bisnis Anda di mana saja di dunia ini.
       </p>
