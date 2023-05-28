b2abc66bd2d57b08c6caf52bf0bee3f0ad549be5
sparda45 <dyazkurosaki@gmail.com>
2023-05-29 01:03:49 +0700

    commit f

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div class="container">
+    <div>
     <div>
       <div class="max-w-xl mx-auto text-center mb-160">
         <h4 class="font-semibold text-lg text-primary mb-2">
@@ -25,7 +25,7 @@ const Form = () => {
         <div class="w-full px-4 mb-8">
           <label for="Email" class="text-base font-bold uppercase text-blue">Email</label>
           <input type="Email" id="Email" class="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-blue focus:ring-1 focus:border-blue"/>
-          <input type="Email" id="Email" class="w-full bg-slate-200 text-dark p-3 rounded-md focus:outline-none focus:ring-blue focus:ring-1 focus:border-blue"/>
+
         </div>
         <div class="w-full px-4 mb-8">
           <label for="Message" class="text-base font-bold uppercase text-blue">Message</label>
@@ -36,7 +36,7 @@ const Form = () => {
         </div>
       </div>
       </form>
-    </div>
+ </div>
   )
 }
 
